708b81e7996535076209b991fd77c469ee52cf80
Bryan Sosa <bry.sosa@outlook.com>
2023-06-14 21:04:42 -0400

    displays taks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  addingEventListener()
+}
+
+function addingEventListener() {
+    document
+    .getElementById("create-task-form")
+    .addEventListener("submit", handleFormSubmit)
+  }
+
+  function handleFormSubmit(e){
+    e.preventDefault()
+    const task = e.target(0).value
+    displayTask(task)
+  }
+
+  function displayTask(task) {
+    const ulTask = document.getElementById("tasks")
+    const liTask = document.createElement("li")
+    liTask.textContent = task
+    ulTask.appendChild(liTask)
+
+  }
